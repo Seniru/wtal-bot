b0a85ff5ab58fc0d8cc6c86966b06903c0839ded
Seniru Pasan Indira <senirupasan@gmail.com>
2019-12-13 11:47:41 +0000

    Supported fetching logs and retrieving rank updates

--- bot.lua
+++ bot.lua
@@ -1,18 +1,70 @@
 local discordia = require('discordia')
 local http = require('coro-http')
-local fromage = require('fromage')
+local api = require('fromage')
 local timer = require('timer')
+local utils = require('utils')
 
-local client = discordia.Client()
+local dClient = discordia.Client()
+local fClient = api()
 
-client:on('messageCreate', function(msg) 
-    if msg.content:lower() == '> ping' then
-        msg:reply('Pong!')
+local id = 1051168
+local updated = 1575632902000
+local histLogs = {}
+
+coroutine.wrap(function()
+    
+    print('Logging...')
+    fClient.connect('Mouseclick1#0000', os.getenv('FORUM_PASSWORD'))
+
+    dClient:on('messageCreate', function(msg) 
+        if msg.content:lower() == '> ping' then
+            msg:reply('Pong!')
+        end
+    end)
+
+    
+    updateRanks(fetchChanges())
+
+    --[[timer.setInterval(1000, function()
+        if history[1].timestamp > updated then
+            local log = 1
+            while not history[log].timestamp == updated do
+                print(history[log].timestamp)
+                log = log + 1
+            end
+            updated = history[log].timestamp
+        end 
+    end)]]
+end)()
+
+function fetchChanges()
+    print('Fetching history')
+    local history = fClient.getTribeHistory(id)
+    local toUpdate = {}
+    if history[1].timestamp > updated then
+        print('History has updated!')
+        local log = 1
+        while not (history[log].timestamp == updated) do
+            local name, rank = getRankUpdate(history[log].log)
+            if name then
+                toUpdate[name] = rank
+            end                
+            log = log + 1
+        end
+        updated = history[log].timestamp
     end
-end)
+    return toUpdate
+end
 
-timer.setInterval(1000, function()
-    print('lol')
-end)
+function updateRanks(logs)
+    for k, v in next, logs do
+        print(k .. ' ' .. v)
+    end
+end
 
-client:run('Bot ' .. os.getenv('DISCORD'))
+function getRankUpdate(log)
+    return log:match('.* has changed the rank of (.+#%d+) to (.+)')
+
+end
+
+dClient:run('Bot ' .. os.getenv('DISCORD'))
