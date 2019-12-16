28bc5feb5ef97bc1126468cd40739935aad78baa
Seniru Pasan Indira <senirupasan@gmail.com>
2019-12-16 06:50:30 +0000

    experimenting

--- package.lua
+++ package.lua
@@ -3,8 +3,8 @@ return {
   version = "1.0.0",
   description = "A discord bot to update transformice roles in the WTAL server",
   dependencies = {
-    "luvit/luvit",
-    "SinisterRectus/discordia",
-    "Lautenschlager-id/fromage"
+    "luvit/luvit"
+    --"SinisterRectus/discordia",
+    --"Lautenschlager-id/fromage"
   },
 }
