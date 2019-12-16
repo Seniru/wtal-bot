0ed0a0f77d820de3cb92608da8c2f18c8dc3fd77
Seniru Pasan Indira <senirupasan@gmail.com>
2019-12-16 04:12:40 +0000

    Configured heroku

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+  name = "Seniru/role-updater",
+  version = "1.0.0",
+  description = "A discord bot to update transformice roles in the WTAL server",
+  dependencies = {
+    "luvit/luvit",
+    "SinisterRectus/discordia",
+    "Lautenschlager-id/fromage"
+  },
+}
