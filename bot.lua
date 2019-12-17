a1980c1f6e68c7c8db505005d1ecd2c20a89158d
Seniru Pasan Indira <senirupasan@gmail.com>
2019-12-17 05:30:47 +0000

    fix: Fixed popping in and out of roles for bot users

--- bot.lua
+++ bot.lua
@@ -42,7 +42,7 @@ coroutine.wrap(function()
 
     dClient:on('memberUpdate', function(member)
         print('Member Update event fired!')
-        if not member:hasRole(enum.roles[members[getStoredName(member.nickname)]]) then
+        if not member.user.bot and not member:hasRole(enum.roles[members[getStoredName(member.nickname)]] or enum.roles['Passer-by']) then
             setRank(member)
         end
     end)
